622182076acc4edd77d8674a83c62eddf3cbd910
Ben Sutherland <bensutherland7@gmail.com>
2016-06-28 10:24:06 -0400

    Include max length

--- 01_scripts/fasta_summarize.sh
+++ 01_scripts/fasta_summarize.sh
@@ -8,35 +8,33 @@
 INPUT="$1"
 LENGTH="$2"
 
-# Begin
-echo "Processing $INPUT"
-
 # Do not change
 TRIMMED_INPUT="$INPUT"_minlength500.fa
 
-# Run summarization for the full file
-echo "Working on full file "$INPUT""
+# Characterize full file
+echo " ** n50 calculation for $INPUT"
+./01_scripts/00_utility_scripts/fasta_n50.py $INPUT
 
-echo "n50 calculation for $INPUT"
-~/01_scripts/fasta_n50.py $INPUT
-
-echo "Number records in $INPUT"
+echo " ** Number records in $INPUT"
 grep -c '>' $INPUT
 
-echo "Full length of $INPUT"
+echo " ** Full length of $INPUT"
 grep -v '>' $INPUT | wc -c
 
+echo " ** Longest contig"
+./01_scripts/00_utility_scripts/fasta_max_len.py $INPUT
 
-# Prepare min length file
-echo "Limiting fasta to only those records > $LENGTH"
-~/01_scripts/fasta_minlength.pl $LENGTH $INPUT > $TRIMMED_INPUT
+# Limit file to records greater than $LENGTH 
+echo " ** minimum length = $LENGTH"
+./01_scripts/00_utility_scripts/fasta_minlength.pl $LENGTH $INPUT > $TRIMMED_INPUT
 
 # Run summarization for trimmed file
-echo "n50 calculation for $TRIMMED_INPUT"
-~/01_scripts/fasta_n50.py $TRIMMED_INPUT
+echo " ** n50 calculation for $TRIMMED_INPUT"
+./01_scripts/00_utility_scripts/fasta_n50.py $TRIMMED_INPUT
 
-echo "Number records in $TRIMMED_INPUT"
+echo " ** Number records in $TRIMMED_INPUT"
 grep -c '>' $TRIMMED_INPUT
 
-echo "Full length of $TRIMMED_INPUT"
+echo " ** Full length of $TRIMMED_INPUT"
 grep -v '>' $TRIMMED_INPUT | wc -c
+
