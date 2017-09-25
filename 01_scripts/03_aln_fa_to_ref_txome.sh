c9f54cb9bfeae9b792f627dc8b4dce852199327a
bensutherland <bensutherland7@gmail.com>
2017-09-25 11:57:40 -0700

    Add log file output to alignment script

--- 01_scripts/03_aln_fa_to_ref_txome.sh
+++ 01_scripts/03_aln_fa_to_ref_txome.sh
@@ -12,26 +12,30 @@ RESULT_FOLDER="04_mapping_results"
 NUM_THREADS="10"
 
 # troubleshoot
-echo $REFERENCE
 OUTPUT_FOLDER=$(basename $REFERENCE)
+LOG_FILE=$OUTPUT_FOLDER"_log.txt"
+echo $REFERENCE 2>&1 | tee -a $LOG_FILE 
 
 # Map reads
 ls -1 $DATA_FOLDER/*.fq |
 	sort -u |
 	while read i
 	do
-	  echo $i
+	  echo $i 2>&1 | tee -a $LOG_FILE 
 	  name=$(basename $i)
 	  label=$(echo $name | perl -pe 's/\.fa//')
 	  ID="@RG\tID:${label}\tSM:${label}\tPL:Illumina"
-	  bowtie2 --end-to-end -k 40 --threads $NUM_THREADS --rg-id $ID -x $REFERENCE --interleaved $i -S $i.bowtie2.sam
+
+          # Alignment
+	  bowtie2 --end-to-end -k 40 --threads $NUM_THREADS --rg-id $ID -x $REFERENCE --interleaved $i -S $i.bowtie2.sam 2>&1 | tee -a $LOG_FILE
 	  samtools view -Sb -f 2 $i.bowtie2.sam > $i.bowtie2.unsorted.bam
 	  #samtools sort -n -o $i.bowtie2.sorted.bam $i.bowtie2.unsorted.bam
 	  #samtools index $i.bowtie2.sorted.bam # note, if samtools sort -n is used, cannot index
-      #rm $i.bowtie2.sam
+
+          # Clean up
+          rm $i.bowtie2.sam
           mkdir $RESULT_FOLDER/$OUTPUT_FOLDER
-          mv *.bam $RESULT_FOLDER/$OUTPUT_FOLDER
+          mv $DATA_FOLDER/*.bam $RESULT_FOLDER/$OUTPUT_FOLDER
 done
 
-# clean up space
-
+mv $LOG_FILE $RESULT_FOLDER
