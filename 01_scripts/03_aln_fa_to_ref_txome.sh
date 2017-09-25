a0735d878669d96a9c832550aa8e3d11b48039f1
bensutherland <bensutherland7@gmail.com>
2017-09-25 11:37:44 -0700

    Update Simple_reads_to_counts mapping script to map fq against ref_txome

--- 01_scripts/03_aln_fa_to_ref_txome.sh
+++ 01_scripts/03_aln_fa_to_ref_txome.sh
@@ -2,23 +2,21 @@
 # Map a set of fasta files against a set of metatranscriptome assemblies
 # Note: Requires that reference is indexed by bowtie2 (see README.md) 
 
-# define usage function
-usage(){
-    echo "Usage: $0 <your_assembly.fasta>"
-    exit 1
-}
-
-
 # Global variables
 DATA_FOLDER="00_input_files"
 REF_FOLDER="02_assemblies"
 REFERENCE="$1"
+RESULT_FOLDER="04_mapping_results"
 
 # User variables
 NUM_THREADS="10"
 
+# troubleshoot
+echo $REFERENCE
+OUTPUT_FOLDER=$(basename $REFERENCE)
+
 # Map reads
-ls -1 $DATA_FOLDER/*.fa |
+ls -1 $DATA_FOLDER/*.fq |
 	sort -u |
 	while read i
 	do
@@ -26,11 +24,13 @@ ls -1 $DATA_FOLDER/*.fa |
 	  name=$(basename $i)
 	  label=$(echo $name | perl -pe 's/\.fa//')
 	  ID="@RG\tID:${label}\tSM:${label}\tPL:Illumina"
-	  bowtie2 --end-to-end -f --interleaved -k 40 --threads $NUM_THREADS --rg-id $ID -x $REFERENCE -U $i -S $i.bowtie2.sam
+	  bowtie2 --end-to-end -k 40 --threads $NUM_THREADS --rg-id $ID -x $REFERENCE --interleaved $i -S $i.bowtie2.sam
 	  samtools view -Sb -f 2 $i.bowtie2.sam > $i.bowtie2.unsorted.bam
 	  #samtools sort -n -o $i.bowtie2.sorted.bam $i.bowtie2.unsorted.bam
 	  #samtools index $i.bowtie2.sorted.bam # note, if samtools sort -n is used, cannot index
       #rm $i.bowtie2.sam
+          mkdir $RESULT_FOLDER/$OUTPUT_FOLDER
+          mv *.bam $RESULT_FOLDER/$OUTPUT_FOLDER
 done
 
 # clean up space
